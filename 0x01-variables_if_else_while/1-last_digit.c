3a3ffccab759a739457756bf6a026b085cc475c7
Francis Akpotu <akpotu.francis@outlook.com>
2023-08-11 15:59:30 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,22 +12,11 @@ int main(void)
 
 	srand (time(0));
 	n = rand() - RAND_MAX / 2;
-	int lastDigit = rand() % 10;
-
-	printf("The string Last digit of %d is ", n);
-
-	if (lastDigit > 5)
-	{
-		printf("and is greater than 5\n");
-	}
-	else if (lastDigit == 0)
-	{
-		printf("and is 0\n");
-	}
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
 	else
-	{
-		printf("and is less than 6 and not 0\n");
-	}
-
-	return (0);
+		printf("%d is negative\n", n);
+	return (0)
 }
