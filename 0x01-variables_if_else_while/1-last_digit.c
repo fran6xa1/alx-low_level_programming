3f16cf161e6debb500bc20a28e4435dfcf397f7f
Francis Akpotu <akpotu.francis@outlook.com>
2023-08-11 17:19:53 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,15 +16,15 @@ int main(void)
 	finaldigit = n % 10;
 	if (finaldigit > 5)
 	{
-		printf("and is greater than 5\n");
+		printf("the string and is greater than 5\n");
 	}
 	else if (finaldigit == 0)
 	{
-		printf("and is equal to 0\n");
+		printf("the string and is equal to 0\n");
 	}
 	else
 	{
-		printf("and is less than 6 and not 0\n");
+		printf("the string and is less than 6 and not 0\n");
 	}
 	return (0);
 }
