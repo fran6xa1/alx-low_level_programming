044fb427e49beeb9a75f370e1d0567f38c006ab0
Francis Akpotu <akpotu.francis@outlook.com>
2023-08-11 19:58:13 -0700

    common and capital letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main- This program prints common letters from a to z and capital letters A to Z
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char commonletters, capitalletters;
+
+	for (commonletters = 'a'; commonletters <= 'z'; commonletters++)
+        putchar(commonletters);
+
+	for (capitalletters = 'A'; capitalletters <= 'Z'; capitalletters++)
+       	putchar(capitalletters);
+	
+	putchar('\n');
+	return (0);
+}
