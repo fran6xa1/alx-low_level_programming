41a1fad52de1206885ec50528c14b2fef3fdec93
Francis Akpotu <akpotu.francis@outlook.com>
2023-08-11 18:34:05 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main- This program assigns a random number to the variable n
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+
+	putchar(alpha);
+	putchar('\n');
+	return (0);
+}
