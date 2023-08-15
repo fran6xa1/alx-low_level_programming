72acad352f70f118779f44f42e4ccdb04ebe674d
Francis Akpotu <akpotu.francis@outlook.com>
2023-08-14 22:03:15 -0700

    last

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,30 +6,27 @@
  */
 int main(void)
 {
-	int i = 0;
+	int i, j = 0;
 
 	while (i <= 99)
 	{
-		int j = 0;
-
+		j = i + 1;
 		while (j <= 99)
 		{
-			if (i <= j)
+			putchar((i / 10) + '0');
+			putchar((j % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i != 98 || j != 99)
 			{
-				putchar((i / 10) + '0');
-				putchar((j % 10) + '0');
+				putchar(',');
 				putchar(' ');
-				putchar((j / 10) + '0');
-				putchar((j % 10) + '0');
-				if (i != 98 || j != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
 			}
-			j++;
-		}
-		i++;
+			j++
+	        }
+		i++
 	}
+	putchar("\n");
 	return (0);
 }
