75d342c48d664950f1377556f29a980f2d73b2a8
Francis Akpotu <akpotu.francis@outlook.com>
2023-08-18 17:19:18 -0700

    nested

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+*print_square - Function prints squares
+*@size: parameter
+*
+*Return:0
+*/
+void print_square(int size)
+{
+	int i1, i2;
+	if (size > 0)
+	{
+		for (i1 = 0; i1 < size; i1++)
+		{
+			for (i2 = 0; i2 < (size - 1); i2++)
+				_putchar('#');
+
+			_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
