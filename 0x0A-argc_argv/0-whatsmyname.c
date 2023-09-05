a4ca825d13f70bfbf05acbae9bba8d07ed233269
Francis Akpotu <akpotu.francis@outlook.com>
2023-09-05 13:13:28 -0700

    argc abd argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - This prints the program name followed by a new line
- * @argc: The number of arguments given to the program
- * @argv: An array of pointers to teh arguments
+ * main - Prints the program name, followed by a new line.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * Return: Always 0.
+ * Return: Always o.
  */
- int main(int argc, char *argv[])
- {
-	 if (argc > 0)
-	 {
-		 printf("%s\n", argv[0]);
-	 }
-	 else
-	 {
-		 printf("Cannot determine program name. \n");
-	 }
-	 return (0);
- }
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
